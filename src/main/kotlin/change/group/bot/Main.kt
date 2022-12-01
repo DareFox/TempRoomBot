dbd25b030aec8e1e7f5fb0e61a978b35c0f2f1f3
DareFox <easymalink@gmail.com>
2022-12-02 02:01:16 +0300

    Use temp room extension in Main.kt

--- src/main/kotlin/change/group/bot/Main.kt
+++ src/main/kotlin/change/group/bot/Main.kt
@@ -1,16 +1,18 @@
 package change.group.bot
 
 import change.group.bot.extensions.PingPongExtension
+import change.group.bot.extensions.TempRoomCreatorExtension
 import com.kotlindiscord.kord.extensions.ExtensibleBot
 import dev.kord.gateway.Intents
 import dev.kord.gateway.PrivilegedIntent
+import org.litote.kmongo.KMongo
 
 @OptIn(PrivilegedIntent::class)
 suspend fun main(args: Array<String>) {
     val bot = ExtensibleBot(Config.botToken) {
         // To add extensions, you need to pass reference to extension class via :: symbol
         extensions {
-            add(::PingPongExtension)
+            add(::TempRoomCreatorExtension)
         }
         intents {
             +Intents.all
