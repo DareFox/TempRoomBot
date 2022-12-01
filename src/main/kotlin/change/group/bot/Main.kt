e76d61a6600d1b3b770d315ee54e893401d1dc16
DareFox <easymalink@gmail.com>
2022-12-01 21:45:54 +0300

    Use intents

--- src/main/kotlin/change/group/bot/Main.kt
+++ src/main/kotlin/change/group/bot/Main.kt
@@ -2,12 +2,18 @@ package change.group.bot
 
 import change.group.bot.extensions.PingPongExtension
 import com.kotlindiscord.kord.extensions.ExtensibleBot
+import dev.kord.gateway.Intents
+import dev.kord.gateway.PrivilegedIntent
 
+@OptIn(PrivilegedIntent::class)
 suspend fun main(args: Array<String>) {
     val bot = ExtensibleBot(Config.botToken) {
         // To add extensions, you need to pass reference to extension class via :: symbol
         extensions {
             add(::PingPongExtension)
+        }
+        intents {
+            +Intents.all
         }
     }
     bot.start()
