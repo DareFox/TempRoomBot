35e26fcc2f101916766c6ce2d11150a1351d726e
DareFox <easymalink@gmail.com>
2022-12-02 03:09:42 +0300

    Add extensions function requring permissions for command

---
+++ src/main/kotlin/change/group/bot/util/RequirePermissions.kt
@@ -0,0 +1,27 @@
+package change.group.bot.util
+
+import com.kotlindiscord.kord.extensions.commands.Arguments
+import com.kotlindiscord.kord.extensions.commands.application.slash.EphemeralSlashCommandContext
+import com.kotlindiscord.kord.extensions.types.respond
+import dev.kord.common.entity.Permission
+import dev.kord.rest.builder.message.create.embed
+
+/**
+ * Require these permissions to run slash command
+ *
+ * Returns **NULL** if requirements are not met.
+ * Useful for fast exit via `?: return`
+ */
+suspend fun <T : Arguments> EphemeralSlashCommandContext<T>.requirePermissions(list: Set<Permission>): Unit? {
+    val sufficientPermissions = member?.asMember()?.getPermissions()?.values?.containsAll(list) ?: false
+
+    if (!sufficientPermissions) {
+        respond {
+            embed {
+                title = "You don't have permissions to run this command."
+            }
+        }
+    }
+
+    return if (sufficientPermissions) Unit else null
+}
